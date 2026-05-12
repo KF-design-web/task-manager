d18a1902825361553a2da63809cbe9f751e02c9f
KF-design-web <kengne.fongang@ictuniversity.edu.cm>
2026-05-12 05:11:55 +0100

    feat: add task creation form with two-way binding

--- src/app/task-list/task-list.ts
+++ src/app/task-list/task-list.ts
@@ -1,14 +1,17 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-task-list',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './task-list.html',
   styleUrl: './task-list.css'
 })
 export class TaskListComponent {
+  newTaskName = '';
+
   tasks = [
     { id: 1, name: 'Learn TypeScript', completed: true },
     { id: 2, name: 'Setup Angular project', completed: true },
@@ -16,4 +19,16 @@ export class TaskListComponent {
     { id: 4, name: 'Connect to Spring Boot API', completed: false },
     { id: 5, name: 'Add authentication', completed: false },
   ];
+
+  addTask() {
+    if (this.newTaskName.trim() === '') return;
+    
+    this.tasks.push({
+      id: this.tasks.length + 1,
+      name: this.newTaskName,
+      completed: false
+    });
+
+    this.newTaskName = '';
+  }
 }
